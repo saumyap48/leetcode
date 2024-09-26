94ab537fcb12aea3de82389069e5b5cfa6dbe11c
Saumya Pandey <saumyap271@gmail.com>
2024-09-26 19:48:51 +0530

    Time: 0 ms (100%), Space: 7.7 MB (95.18%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        int n=s.size();        
+        string result="";
+        for(int i=0; i<n; i++){
+         if(s[i]!=']'){
+            result.push_back(s[i]);
+         }
+         else{
+            string str="";
+            while(!result.empty() && result.back()!='['){str.push_back(result.back());
+       result.pop_back();
+            }
+            reverse(str.begin(),str.end());
+            result.pop_back();
+            string nums="";
+            while(!result.empty() && result.back()>='0' && result.back()<='9'){
+                nums.push_back(result.back());
+                result.pop_back();
+            }
+            reverse(nums.begin(),nums.end());
+            int int_num =stoi(nums);
+            while(int_num){
+                result+=str;
+                int_num--;
+            }
+         }
+        }
+        return result;
+    }
+};
