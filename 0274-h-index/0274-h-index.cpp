3955fb18637be04317056504842d68dd427b1d6f
Saumya Pandey <saumyap271@gmail.com>
2024-09-28 12:30:36 +0530

    Time: 2 ms (53.98%), Space: 11.1 MB (71.34%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-    sort(citations.begin(), citations.end());
-    int n = citations.size();
-    for (int i = 0; i < n; ++i) {
-     if (citations[n - i - 1] < i + 1) {
-            return i; 
+        sort(citations.begin(), citations.end());
+        int n = citations.size(),count=0;
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                count++; 
+            }
         }
+        return count++;
     }
-    return n; 
-}
 };
