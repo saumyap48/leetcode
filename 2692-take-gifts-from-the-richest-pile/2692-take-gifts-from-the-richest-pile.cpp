bc1f2612a54e19c0d243dcaff60e4e393edd0130
Saumya Pandey <saumyap271@gmail.com>
2024-12-12 16:38:06 +0530

    Time: 0 ms (100%), Space: 13.4 MB (16.26%) - LeetHub

---
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int>pq(begin(gifts),end(gifts));
+        long long sum=0;
+        for(int &gifts : gifts){
+            sum+=gifts;
+        }
+        while(k--){
+            int maxEl=pq.top();
+            pq.pop();
+            int remaining=sqrt(maxEl);
+            sum-=(maxEl-remaining);
+            pq.push(remaining);
+        }
+        return sum;
+    }
+};
