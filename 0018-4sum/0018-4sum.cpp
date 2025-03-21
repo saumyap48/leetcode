e846762cd90bbd3f2a8a2f312862b35e1f3af5ba
Saumya Pandey <saumyap271@gmail.com>
2025-03-21 18:08:39 +0530

    Time: 23 ms (56.23%), Space: 17.6 MB (32.37%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+     vector<vector<int>>answer;
         int n=nums.size();
-        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        for(int i=0; i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1])
+            continue;
             for(int j=i+1; j<n; j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int k=j+1;
-                int l=n-1;
-                while(k<l){
-                    long long sum=nums[i];
-                    sum+=nums[j];
-                    sum+=nums[k];
-                    sum+=nums[l];
-                    if(sum==target){
-                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
-                      ans.push_back(temp);
-                      k++; l--; 
-                      while(k<l && nums[k]==nums[k-1])k++;
-                      while(k<l && nums[l]==nums[l+1])l--; 
+                if(j>i+1 &&nums[j]==nums[j-1])
+                continue;
+                int start=j+1;
+                int end=n-1;
+                while(start<end){
+        long long totalsum=(long long)nums[i]+nums[j]+nums[start]+nums[end];
+        if(totalsum==target){
+        vector<int>temp={nums[i],nums[j],nums[start],nums[end]};
+            answer.push_back(temp);
+            start++;
+            end--;
+        while(start<end &&nums[start]==nums[start-1])start++;
+        while(start<end && nums[end]==nums[end+1])end--;
                     }
-                    else if(sum<target)k++;
-                    else l--;
+                    else if(totalsum<target){
+                        start++;
+                    }
+                    else{
+                        end--;
+                    }
                 }
             }
         }
-        return ans;
+        return answer;
     }
 };
