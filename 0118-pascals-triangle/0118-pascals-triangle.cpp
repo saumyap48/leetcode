c42c357fa64712cfdc33a386ccb4a4f2efc31959
Saumya Pandey <saumyap271@gmail.com>
2025-03-22 16:41:28 +0530

    Time: 0 ms (100%), Space: 9.6 MB (89.67%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
+    vector<int> generatedRow(int row) {
+        long long answer = 1;
+        vector<int> answerRow;
+        answerRow.push_back(1);
+        
+        for (int col = 1; col < row; col++) {
+            answer = answer * (row - col);
+            answer = answer / col;
+            answerRow.push_back(answer);
+        }
+        return answerRow;
+    }
+
     vector<vector<int>> generate(int n) {
-     vector<vector<int>>answer(n);
-     for(int i=0; i<n; i++){
-         answer[i].assign(i+1,1);
-         for(int j=1; j<i; j++){
-            answer[i][j]=answer[i-1][j-1]+answer[i-1][j];
-         }
-     }
-        return answer;
+        vector<vector<int>> pascalTriangle;
+        for (int row = 1; row <= n; row++) {
+            pascalTriangle.push_back(generatedRow(row));
+        }
+        return pascalTriangle;
     }
 };
