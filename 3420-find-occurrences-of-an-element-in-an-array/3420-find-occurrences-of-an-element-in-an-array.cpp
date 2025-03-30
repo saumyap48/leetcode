e3344413d90065b84ee1ad0514efa9165c52e57d
Saumya Pandey <saumyap271@gmail.com>
2025-03-30 21:24:44 +0530

    Time: 50 ms (14.81%), Space: 190.6 MB (23.7%) - LeetHub

--- 3420-find-occurrences-of-an-element-in-an-array/3420-find-occurrences-of-an-element-in-an-array.cpp
+++ 3420-find-occurrences-of-an-element-in-an-array/3420-find-occurrences-of-an-element-in-an-array.cpp
@@ -1,23 +1,28 @@
-class Solution {
+class Solution{
 public:
-    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
-        int n=nums.size();
-        unordered_map<int,int>mp;
-        int count=0;
-        for(int i=0; i<n; i++){
-            if(nums[i]==x){
-                count++;
-                mp[count]=i;
-            }
+vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries,int x) {
+    int n = nums.size();
+    unordered_map<int, int> mp;
+    int count = 0;
+    vector<int> answer;  // New vector to store results
+
+    // Storing occurrences of x
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == x) {
+            count++;
+            mp[count] = i;
         }
-        for(int i=0; i<queries.size(); i++){
-            if(mp.find(queries[i])!=mp.end()){
-                queries[i]=mp[queries[i]];
-            }
-            else{
-                queries[i]=-1;
-            }
-        }
-        return queries;
     }
+
+    // Processing queries and storing results in 'answer'
+    for (int i = 0; i < queries.size(); i++) {
+        if (mp.find(queries[i]) != mp.end()) {
+            answer.push_back(mp[queries[i]]);
+        } else {
+            answer.push_back(-1);
+        }
+    }
+
+    return answer;  // Return the new vector instead of modifying queries
+}
 };
