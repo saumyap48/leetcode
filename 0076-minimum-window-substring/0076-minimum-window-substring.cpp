58a73e1d3c7aea701cc286c828af3cb48007b87c
Saumya Pandey <saumyap271@gmail.com>
2025-05-20 14:41:22 +0530

    Time: 0 ms (100%), Space: 11.9 MB (11.23%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n=s.size();
-        int p=t.size();
-        int r=0;
-        int l=0;
-        int sIndex=-1;
-        int count=0;
         int minlength=INT_MAX;
-        unordered_map<char,int>m;
-        for(int i=0; i<p; i++)
-            m[t[i]]++;
-            while(r<s.size()){
-                if(m[s[r]]>=1)
-                count=count+1;
-                m[s[r]]--;
-                while(count==p){
-                    if(r-l+1<minlength)
-                    {
-                    sIndex=l;
-                    minlength=r-l+1;
-                }
-                    m[s[l]]++;
-                    if(m[s[l]]>=1)
-                    count=count-1;
-                l++;
-              }
-              r++;
-}
-    if(minlength==INT_MAX)return "";
-    else return s.substr(sIndex,minlength);
+        int sIndex=-1;
+        int l=0,r=0;
+        int hash[256]={0};
+        for(char c: t){
+            hash[c]++;
+        }
+        int count=0;
+        while(r<s.size()){
+         if(hash[s[r]]>0){
+            count++;
+         }
+         hash[s[r]]--;
+         while(count==t.size()){
+           if(r-l+1<minlength){
+            minlength=r-l+1;
+            sIndex=l;
+           }
+           hash[s[l]]++;
+           if(hash[s[l]]>0){
+            count--;
+           }
+           l++;
+         }
+         r++;
+        }
+        return (sIndex==-1) ? "" : s.substr(sIndex,minlength);
     }
 };
