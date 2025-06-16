2f7eb84d4fed8cb97187bf37e8bc8bf2eb4303d1
Saumya Pandey <saumyap271@gmail.com>
2025-06-16 23:24:55 +0530

    Time: 3 ms (24.29%), Space: 11.7 MB (99.66%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         int maxi=0;
         int answer=-1;
-        for(int i=0; i<n-1; i++){
+        for(int i=0; i<n; i++){
         for(int j=i+1; j<n; j++){
             if(nums[i]>nums[j]){
                 continue;
