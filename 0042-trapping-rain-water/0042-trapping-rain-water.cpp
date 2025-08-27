241e54da4c2cb7a1c8f4dc45a35a76ce5bb34484
Saumya Pandey <saumyap271@gmail.com>
2025-08-27 11:34:54 +0530

    Time: 4 ms (10.72%), Space: 27.4 MB (20.19%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,32 @@
-class Solution {
-public:
-  vector<int>getLeftMaxArray(vector<int>&height, int& n){
-   vector<int> leftmax(n);
-   leftmax[0]=height[0];
-   for(int i=1; i<n; i++){
-   leftmax[i]=max(leftmax[i-1],height[i]);
-   }
-   return leftmax;
-  }
-
- vector<int>getRightMaxArray(vector<int>&height, int& n){
-      vector<int>rightmin(n);
-      rightmin[n-1]=height[n-1];
-      for(int i=n-2; i>=0; i--){
-        rightmin[i]=max(rightmin[i+1],height[i]);
-      }
-      return rightmin;
-  }
-
-
- 
-    int trap(vector<int>& height) {
-        int n=height.size();
-        vector<int>leftmax=getLeftMaxArray(height,n);
-        vector<int>rightmax=getRightMaxArray(height,n);
-        int sum=0;
-        for(int i=0; i<n; i++){
-       int h=min(leftmax[i],rightmax[i])-height[i];
-       sum+=h;
+class Solution
+{
+    vector<int>findPrefixMax(vector<int>&arr,int &n){
+        vector<int>prefixMax(n);
+        prefixMax[0]=arr[0];
+        for(int i=1; i<n; i++){
+            prefixMax[i]=max(prefixMax[i-1],arr[i]);
         }
-        return sum;
+        return prefixMax;
+    }
+        vector<int>findSuffixMax(vector<int>&arr,int &n){
+        vector<int>suffixMax(n);
+        suffixMax[n-1]=arr[n-1];
+        for(int i=n-2; i>=0; i--){
+            suffixMax[i]=max(suffixMax[i+1],arr[i]);
+        }
+        return suffixMax;
+    }
+public:
+    int trap(vector<int> &height){
+        int n=height.size();
+        int total=0;
+        vector<int>leftMax=findPrefixMax(height,n);
+        vector<int>rightMax=findSuffixMax(height,n);
+        for(int i=0; i<n; i++){
+            if(height[i]<leftMax[i] && height[i]<rightMax[i]){
+                total+=(min(leftMax[i],rightMax[i])-height[i]);
+            }
+        }
+        return total;
     }
 };
